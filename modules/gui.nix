b9d963464a7e0eef5d9d6dc1c5d3927c4039d6cb
Jared Baur <jaredbaur@fastmail.com>
2022-07-02 13:22:52 -0700

    Remove GTK portal

--- modules/gui.nix
+++ modules/gui.nix
@@ -66,9 +66,6 @@ with lib;
     xdg.portal = {
       enable = true;
       wlr.enable = true;
-      # gtk portal needed to make gtk apps happy
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-      gtkUsePortal = true;
     };
 
     fonts.fonts = [ pkgs.iosevka-bin ];
