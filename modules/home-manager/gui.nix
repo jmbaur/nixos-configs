f7d6124b735026ef9ef2fa93c28239e2d47a18a3
Jared Baur <jaredbaur@fastmail.com>
2022-07-03 07:12:07 -0700

    Add resize mode bindings

--- modules/home-manager/gui.nix
+++ modules/home-manager/gui.nix
@@ -148,150 +148,176 @@ with lib; {
 
     wayland.windowManager.sway = {
       enable = true;
-      config = {
-        output."*".background = "#222222 solid_color";
-        input =
-          let
-            keyboardSettings = { xkb_options = "ctrl:nocaps"; };
-            touchpadSettings = {
-              dwt = "enabled";
-              middle_emulation = "enabled";
-              natural_scroll = "enabled";
-              tap = "enabled";
+      config =
+        let
+          mod = config.wayland.windowManager.sway.config.modifier;
+        in
+        {
+          output."*".background = "#222222 solid_color";
+          input =
+            let
+              keyboardSettings = { xkb_options = "ctrl:nocaps"; };
+              touchpadSettings = {
+                dwt = "enabled";
+                middle_emulation = "enabled";
+                natural_scroll = "enabled";
+                tap = "enabled";
+              };
+            in
+            {
+              "type:keyboard" = keyboardSettings;
+              "type:touchpad" = touchpadSettings;
+              "113:16461:Logitech_K400_Plus" = keyboardSettings // touchpadSettings;
             };
-          in
-          {
-            "type:keyboard" = keyboardSettings;
-            "type:touchpad" = touchpadSettings;
-            "113:16461:Logitech_K400_Plus" = keyboardSettings // touchpadSettings;
+          assigns = {
+            "7" = [{ title = "pipe:xwayland-mirror"; }];
           };
-        assigns = {
-          "7" = [{ title = "pipe:xwayland-mirror"; }];
-        };
-        window.commands = [
-          {
-            criteria.app_id = "^chrome-.*__.*-Default$";
-            command = "shortcuts_inhibitor disable";
-          }
-          {
-            criteria.title = "Firefox — Sharing Indicator";
-            command = "kill";
-          }
-        ];
-        floating.criteria = [
-          { title = "^(Zoom Cloud Meetings|zoom)$"; }
-          { title = "Picture-in-Picture"; }
-          { title = "pipe:xwayland-mirror"; }
-        ];
-        fonts = {
-          names = [ config.programs.kitty.font.name ];
-          size = 12.0;
-          style = "Regular";
-        };
-        terminal = "${pkgs.kitty}/bin/kitty";
-        menu = "${pkgs.bemenu}/bin/bemenu-run";
-        modifier = "Mod4";
-        window = {
-          hideEdgeBorders = "smart";
-          titlebar = true;
-        };
-        defaultWorkspace = "workspace number 1";
-        focus.forceWrapping = true;
-        keybindings =
-          let
-            mod = config.wayland.windowManager.sway.config.modifier;
-          in
-          {
-            "${mod}+0" = "workspace number 10";
-            "${mod}+1" = "workspace number 1";
-            "${mod}+2" = "workspace number 2";
-            "${mod}+3" = "workspace number 3";
-            "${mod}+4" = "workspace number 4";
-            "${mod}+5" = "workspace number 5";
-            "${mod}+6" = "workspace number 6";
-            "${mod}+7" = "workspace number 7";
-            "${mod}+8" = "workspace number 8";
-            "${mod}+9" = "workspace number 9";
-            "${mod}+Control+space" = "exec ${pkgs.mako}/bin/makoctl dismiss --all";
-            "${mod}+Down" = "focus down";
-            "${mod}+Left" = "focus left";
-            "${mod}+Return" = "exec ${config.wayland.windowManager.sway.config.terminal}";
-            "${mod}+Right" = "focus right";
-            "${mod}+Shift+0" = "move container to workspace number 10";
-            "${mod}+Shift+1" = "move container to workspace number 1";
-            "${mod}+Shift+2" = "move container to workspace number 2";
-            "${mod}+Shift+3" = "move container to workspace number 3";
-            "${mod}+Shift+4" = "move container to workspace number 4";
-            "${mod}+Shift+5" = "move container to workspace number 5";
-            "${mod}+Shift+6" = "move container to workspace number 6";
-            "${mod}+Shift+7" = "move container to workspace number 7";
-            "${mod}+Shift+8" = "move container to workspace number 8";
-            "${mod}+Shift+9" = "move container to workspace number 9";
-            "${mod}+Shift+Down" = "move down";
-            "${mod}+Shift+Left" = "move left";
-            "${mod}+Shift+Right" = "move right";
-            "${mod}+Shift+Up" = "move up";
-            "${mod}+Shift+b" = "bar mode toggle";
-            "${mod}+Shift+c" = "reload";
-            "${mod}+Shift+e" = "exec ${pkgs.sway}/bin/swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' '${pkgs.sway}/bin/swaymsg exit'";
-            "${mod}+Shift+h" = "move left";
-            "${mod}+Shift+j" = "move down";
-            "${mod}+Shift+k" = "move up";
-            "${mod}+Shift+l" = "move right";
-            "${mod}+Shift+minus" = "move scratchpad";
-            "${mod}+Shift+p" = "exec ${pkgs.bitwarden-bemenu}/bin/bitwarden-bemenu";
-            "${mod}+Shift+q" = "kill";
-            "${mod}+Shift+r" = "restart";
-            "${mod}+Shift+s" = "sticky toggle";
-            "${mod}+Shift+space" = "floating toggle";
-            "${mod}+Tab" = "workspace back_and_forth";
-            "${mod}+Up" = "focus up";
-            "${mod}+a" = "focus parent";
-            "${mod}+b" = "split h";
-            "${mod}+c" = "exec ${pkgs.clipman}/bin/clipman pick --tool=bemenu";
-            "${mod}+e" = "layout toggle split";
-            "${mod}+f" = "fullscreen toggle";
-            "${mod}+h" = "focus left";
-            "${mod}+j" = "focus down";
-            "${mod}+k" = "focus up";
-            "${mod}+l" = "focus right";
-            "${mod}+minus" = "scratchpad show";
-            "${mod}+p" = "exec ${config.wayland.windowManager.sway.config.menu}";
-            "${mod}+r" = "mode resize";
-            "${mod}+s" = "layout stacking";
-            "${mod}+space" = "focus mode_toggle";
-            "${mod}+v" = "split v";
-            "${mod}+w" = "layout tabbed";
-            "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
-            "XF86AudioMicMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-            "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
-            "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
-            "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
-            "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
+          window.commands = [
+            {
+              criteria.app_id = "^chrome-.*__.*-Default$";
+              command = "shortcuts_inhibitor disable";
+            }
+            {
+              criteria.title = "Firefox — Sharing Indicator";
+              command = "kill";
+            }
+          ];
+          floating.criteria = [
+            { title = "^(Zoom Cloud Meetings|zoom)$"; }
+            { title = "Picture-in-Picture"; }
+            { title = "pipe:xwayland-mirror"; }
+          ];
+          fonts = {
+            names = [ config.programs.kitty.font.name ];
+            size = 12.0;
+            style = "Regular";
           };
-        bars = [{
-          fonts = config.wayland.windowManager.sway.config.fonts;
-          mode = "dock";
-          position = "top";
-          statusCommand = "${pkgs.gobar}/bin/gobar";
-          trayOutput = "*";
+          terminal = "${pkgs.kitty}/bin/kitty";
+          menu = "${pkgs.bemenu}/bin/bemenu-run";
+          modifier = "Mod4";
+          window = {
+            hideEdgeBorders = "smart";
+            titlebar = true;
+          };
+          defaultWorkspace = "workspace number 1";
+          focus.forceWrapping = true;
+          keybindings = {
+            terminal = "${pkgs.kitty}/bin/kitty";
+            menu = "${pkgs.bemenu}/bin/bemenu-run";
+            modifier = "Mod4";
+            window = {
+              hideEdgeBorders = "smart";
+              titlebar = true;
+            };
+            defaultWorkspace = "workspace number 1";
+            focus.forceWrapping = true;
+            keybindings = {
+              "${mod}+0" = "workspace number 10";
+              "${mod}+1" = "workspace number 1";
+              "${mod}+2" = "workspace number 2";
+              "${mod}+3" = "workspace number 3";
+              "${mod}+4" = "workspace number 4";
+              "${mod}+5" = "workspace number 5";
+              "${mod}+6" = "workspace number 6";
+              "${mod}+7" = "workspace number 7";
+              "${mod}+8" = "workspace number 8";
+              "${mod}+9" = "workspace number 9";
+              "${mod}+Control+space" = "exec ${pkgs.mako}/bin/makoctl dismiss --all";
+              "${mod}+Down" = "focus down";
+              "${mod}+Left" = "focus left";
+              "${mod}+Return" = "exec ${config.wayland.windowManager.sway.config.terminal}";
+              "${mod}+Right" = "focus right";
+              "${mod}+Shift+0" = "move container to workspace number 10";
+              "${mod}+Shift+1" = "move container to workspace number 1";
+              "${mod}+Shift+2" = "move container to workspace number 2";
+              "${mod}+Shift+3" = "move container to workspace number 3";
+              "${mod}+Shift+4" = "move container to workspace number 4";
+              "${mod}+Shift+5" = "move container to workspace number 5";
+              "${mod}+Shift+6" = "move container to workspace number 6";
+              "${mod}+Shift+7" = "move container to workspace number 7";
+              "${mod}+Shift+8" = "move container to workspace number 8";
+              "${mod}+Shift+9" = "move container to workspace number 9";
+              "${mod}+Shift+Down" = "move down";
+              "${mod}+Shift+Left" = "move left";
+              "${mod}+Shift+Right" = "move right";
+              "${mod}+Shift+Up" = "move up";
+              "${mod}+Shift+b" = "bar mode toggle";
+              "${mod}+Shift+c" = "reload";
+              "${mod}+Shift+e" = "exec ${pkgs.sway}/bin/swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' '${pkgs.sway}/bin/swaymsg exit'";
+              "${mod}+Shift+h" = "move left";
+              "${mod}+Shift+j" = "move down";
+              "${mod}+Shift+k" = "move up";
+              "${mod}+Shift+l" = "move right";
+              "${mod}+Shift+minus" = "move scratchpad";
+              "${mod}+Shift+p" = "exec ${pkgs.bitwarden-bemenu}/bin/bitwarden-bemenu";
+              "${mod}+Shift+q" = "kill";
+              "${mod}+Shift+r" = "restart";
+              "${mod}+Shift+s" = "sticky toggle";
+              "${mod}+Shift+space" = "floating toggle";
+              "${mod}+Tab" = "workspace back_and_forth";
+              "${mod}+Up" = "focus up";
+              "${mod}+a" = "focus parent";
+              "${mod}+b" = "split h";
+              "${mod}+c" = "exec ${pkgs.clipman}/bin/clipman pick --tool=bemenu";
+              "${mod}+e" = "layout toggle split";
+              "${mod}+f" = "fullscreen toggle";
+              "${mod}+h" = "focus left";
+              "${mod}+j" = "focus down";
+              "${mod}+k" = "focus up";
+              "${mod}+l" = "focus right";
+              "${mod}+minus" = "scratchpad show";
+              "${mod}+p" = "exec ${config.wayland.windowManager.sway.config.menu}";
+              "${mod}+r" = "mode resize";
+              "${mod}+s" = "layout stacking";
+              "${mod}+space" = "focus mode_toggle";
+              "${mod}+v" = "split v";
+              "${mod}+w" = "layout tabbed";
+              "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+              "XF86AudioMicMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+              "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
+              "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+              "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+              "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
+            };
+            modes = {
+              resize = {
+                "${mod}+r" = "mode default";
+                Down = "resize grow height 10 px";
+                Escape = "mode default";
+                Left = "resize shrink width 10 px";
+                Return = "mode default";
+                Right = "resize grow width 10 px";
+                Up = "resize shrink height 10 px";
+                h = "resize shrink width 10 px";
+                j = "resize grow height 10 px";
+                k = "resize shrink height 10 px";
+                l = "resize grow width 10 px";
+              };
+            };
+            bars = [{
+              fonts = config.wayland.windowManager.sway.config.fonts;
+              mode = "dock";
+              position = "top";
+              statusCommand = "${pkgs.gobar}/bin/gobar";
+              trayOutput = "*";
+              extraConfig = ''
+                height ${toString bemenuHeight}
+              '';
+            }];
+            workspaceAutoBackAndForth = true;
+            seat."*" = {
+              hide_cursor = "when-typing enable";
+              xcursor_theme = "${config.home.pointerCursor.name} ${toString config.home.pointerCursor.size}";
+            };
+            startup = [
+              # https://github.com/nix-community/home-manager/issues/2797
+              { command = "${pkgs.systemd}/bin/systemctl --user reload-or-restart kanshi"; }
+            ];
+          };
           extraConfig = ''
-            height ${toString bemenuHeight}
+            include /etc/sway/config.d/*
           '';
-        }];
-        workspaceAutoBackAndForth = true;
-        seat."*" = {
-          hide_cursor = "when-typing enable";
-          xcursor_theme = "${config.home.pointerCursor.name} ${toString config.home.pointerCursor.size}";
         };
-        startup = [
-          # https://github.com/nix-community/home-manager/issues/2797
-          { command = "${pkgs.systemd}/bin/systemctl --user reload-or-restart kanshi"; }
-        ];
-      };
-      extraConfig = ''
-        include /etc/sway/config.d/*
-      '';
     };
   };
 }
