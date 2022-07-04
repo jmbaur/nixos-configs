6a35386e6cd9e732256a2a700bdcb8f6988a95d3
Jared Baur <jaredbaur@fastmail.com>
2022-07-03 18:53:58 -0700

    Add web app for discord

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 {
   default = final: prev: {
     bitwarden-bemenu = prev.callPackage ./bitwarden-bemenu.nix { };
+    discord-webapp = final.lib.mkWebApp "discord" "https://discord.com/app";
     fdroidcl = prev.callPackage ./fdroidcl.nix { };
     j = prev.callPackage ./j.nix { };
     lib = prev.lib // { mkWebApp = prev.callPackage ./mkWebApp.nix { }; };
