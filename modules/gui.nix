a8c79bbec0aa9b9ee84a702c4f10c8ee74458082
Jared Baur <jaredbaur@fastmail.com>
2022-07-10 14:21:50 -0700

    Don't disable QT window decoration

--- modules/gui.nix
+++ modules/gui.nix
@@ -65,7 +65,6 @@ with lib;
         export SDL_VIDEODRIVER=wayland
         # QT (needs qt5.qtwayland in systemPackages):
         export QT_QPA_PLATFORM=wayland-egl
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
         # Fix for some Java AWT applications (e.g. Android Studio),
         # use this if they aren't displayed properly:
         export _JAVA_AWT_WM_NONREPARENTING=1
