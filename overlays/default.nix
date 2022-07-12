75a6acd908c0d6ff0142bf4be0d8048316269248
Jared Baur <jaredbaur@fastmail.com>
2022-07-12 16:51:56 -0700

    Put webapps back

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,10 +1,15 @@
 final: prev: {
   bitwarden-bemenu = prev.callPackage ./bitwarden-bemenu.nix { };
-  discord-webapp = final.lib.mkWebApp "discord" "https://discord.com/app";
   fdroidcl = prev.callPackage ./fdroidcl.nix { };
   j = prev.callPackage ./j.nix { };
   mkWebApp = prev.callPackage ./mkWebApp.nix { };
   mirror-to-x = prev.callPackage ./mirror-to-x.nix { };
   v4l-show = prev.callPackage ./v4l-show.nix { };
   zf = prev.callPackage ./zf.nix { };
+
+  discord-webapp = final.mkWebApp "discord" "https://discord.com/app";
+  outlook-webapp = final.mkWebApp "outlook" "https://outlook.com";
+  slack-webapp = final.mkWebApp "slack" "https://app.slack.com/client";
+  spotify-webapp = final.mkWebApp "spotify" "https://open.spotify.com";
+  teams-webapp = final.mkWebApp "teams" "https://teams.microsoft.com";
 }
