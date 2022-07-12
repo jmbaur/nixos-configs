3d0b782963ff22a0a669a895e82f0dedbd12cb51
Jared Baur <jaredbaur@fastmail.com>
2022-07-12 12:47:09 -0700

    Try vulkan renderer for sway

--- modules/gui.nix
+++ modules/gui.nix
@@ -61,12 +61,15 @@ with lib;
         zathura
       ];
       extraSessionCommands = ''
+        # vulkan renderer support
+        export WLR_RENDERER=vulkan
+        export VK_LAYER_PATH=${pkgs.vulkan-validation-layers}/result/share/vulkan/explicit_layer.d
         # SDL:
         export SDL_VIDEODRIVER=wayland
         # QT (needs qt5.qtwayland in systemPackages):
         export QT_QPA_PLATFORM=wayland-egl
-        # Fix for some Java AWT applications (e.g. Android Studio),
-        # use this if they aren't displayed properly:
+        # Fix for some Java AWT applications (e.g. Android Studio), use this if
+        # they aren't displayed properly:
         export _JAVA_AWT_WM_NONREPARENTING=1
       '';
     };
