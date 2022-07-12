b101b78c80f4e0c711dac1d1eccf5d64fd96354f
Jared Baur <jaredbaur@fastmail.com>
2022-07-12 16:44:06 -0700

    Don't put mkWebApp under lib

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,7 @@ final: prev: {
   discord-webapp = final.lib.mkWebApp "discord" "https://discord.com/app";
   fdroidcl = prev.callPackage ./fdroidcl.nix { };
   j = prev.callPackage ./j.nix { };
-  lib = prev.lib // { mkWebApp = prev.callPackage ./mkWebApp.nix { }; };
+  mkWebApp = prev.callPackage ./mkWebApp.nix { };
   mirror-to-x = prev.callPackage ./mirror-to-x.nix { };
   v4l-show = prev.callPackage ./v4l-show.nix { };
   zf = prev.callPackage ./zf.nix { };
